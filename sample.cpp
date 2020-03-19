1dc59348f73d7c6c8bb3b1e0920237c668fbb227
Reddy, Patilvijayabhaskar <Patilvijayabhaskar.Reddy2@harman.com>
2020-03-19 12:23:18 +0530

    M1

---
+++ sample.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class base {
+public:
+
+};
+
+class derived : public base {
+
+};
+
+int main()
+{
+    base b;
+    derived d;
+
+    
+    return 0;
+}
